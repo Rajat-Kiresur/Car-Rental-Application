584a9415fda58bde892e399d56685e04213b1687
Rajat Kiresur <rajatkiresur07@gmail.com>
2025-03-05 23:27:34 +0530

    Updated AuthModals component

--- src/Components/AuthModals.jsx
+++ src/Components/AuthModals.jsx
@@ -1,26 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './AuthModals.css';
 
-export default function AuthModals() {
-  const [isLoginOpen, setLoginOpen] = useState(false);
-  const [isSignupOpen, setSignupOpen] = useState(false);
-
-  const toggleLogin = () => setLoginOpen(!isLoginOpen);
-  const toggleSignup = () => setSignupOpen(!isSignupOpen);
-
+export default function AuthModals({
+  isLoginOpen,
+  setLoginOpen,
+  isSignupOpen,
+  setSignupOpen,
+}) {
   return (
-    <div className='auth-buttons'>
-      <button className='btn' onClick={toggleLogin}>Login</button>
-      <button className='btn' onClick={toggleSignup}>Signup</button>
-
+    <>
       {isLoginOpen && (
         <div className='modal'>
           <div className='modal-content'>
             <h2>Login</h2>
-            <input type='email' placeholder='Email' className='input'/>
-            <input type='password' placeholder='Password' className='input'/>
+            <input type='email' placeholder='Email' className='input' />
+            <input type='password' placeholder='Password' className='input' />
             <div className='modal-actions'>
-              <button className='btn' onClick={toggleLogin}>Cancel</button>
+              <button className='btn' onClick={() => setLoginOpen(false)}>
+                Cancel
+              </button>
               <button className='btn'>Login</button>
             </div>
           </div>
@@ -31,16 +29,18 @@ export default function AuthModals() {
         <div className='modal'>
           <div className='modal-content'>
             <h2>Signup</h2>
-            <input type='text' placeholder='Name' className='input'/>
-            <input type='email' placeholder='Email' className='input'/>
-            <input type='password' placeholder='Password' className='input'/>
+            <input type='text' placeholder='Name' className='input' />
+            <input type='email' placeholder='Email' className='input' />
+            <input type='password' placeholder='Password' className='input' />
             <div className='modal-actions'>
-              <button className='btn' onClick={toggleSignup}>Cancel</button>
+              <button className='btn' onClick={() => setSignupOpen(false)}>
+                Cancel
+              </button>
               <button className='btn'>Signup</button>
             </div>
           </div>
         </div>
       )}
-    </div>
+    </>
   );
 }
